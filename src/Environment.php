46ab9ccb54116739a0b137450ce12d7a52ddb2d2
Tom van Overloop <tom@xtradigital>
2019-11-13 13:39:49 +0100

    moved env() function to helper

--- src/Environment.php
+++ src/Environment.php
@@ -4,30 +4,22 @@ namespace Xtra\KoPackage;
 
 use Symfony\Component\Dotenv\Dotenv;
 
-class Environment{
-    
+class Environment {
+
     protected static $_instance;
-    
-    public static function instance(){
-        if( !Environment::$_instance  instanceof Dotenv){
-            
-            $file_location = DOCROOT.'/.env';
-            if( ! file_exists($file_location) ){
+
+    public static function instance() {
+        if (!Environment::$_instance instanceof Dotenv) {
+
+            $file_location = DOCROOT . '/.env';
+            if (!file_exists($file_location)) {
                 http_response_code(500);
                 die('environment not available');
             }
             $dotenv = new Dotenv();
-            $dotenv->load(DOCROOT.'/.env');
-            
-            //Environment::$_instance = $dotenv;
+            $dotenv->load(DOCROOT . '/.env');
         }
         return Environment::$_instance;
     }
-    
-}
 
-if( !function_exists('env') ){
-    function env($key){
-        return Arr::get($_ENV, $key, NULL);
-    }
 }
