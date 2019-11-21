2113d52d611db7e252647c5d41e4baa8271157a1
Tom van Overloop <tom@xtradigital>
2019-11-21 16:04:25 +0100

    fix exception path

--- Exception/LocationException.php
+++ Exception/LocationException.php
@@ -4,7 +4,7 @@ namespace Xtra\KoPackage\Environment\Exception;
 
 final class LocationException extends Exception implements Throwable
 {
-    public function __construct(string $path, int $code = 0, \Exception $previous = null)
+    public function __construct(string $path, int $code = 0, \Throwable $previous = null)
     {
         parent::__construct(sprintf('Unable to read the "%s" environment file.', $path), $code, $previous);
     }
